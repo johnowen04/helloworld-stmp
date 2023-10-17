5639edecab3779bda93bf0e52da1b280d5ec8c80
John Owen <johnowen77777@gmail.com>
2023-10-17 23:52:21 +0700

    Latihan 1

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize:50, color:'rgb(255,0,0)' }} >Hello, World!</Text>
+      <Text style={{ fontSize: 50, color: 'rgb(255,0,0)' }} >Hello, World!</Text>
+      <Nilai score='99'></Nilai>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const Nilai = (prop) => {
+  const [action, setAction] = useState(prop.score);
+
+  return (
+    <View>
+      <Text>Skor anda adalah {action} dengan predikat {predikat(action)}</Text>
+      <Button title="-5"
+        onPress={() => {
+          if (action - 5 > 0) {
+            setAction(action * 1 - 5);
+          }
+        }} />
+      <Button title="+5"
+        onPress={() => {
+          if (action + 5 <= 100) {
+            setAction(action * 1 + 5);
+          }
+        }} />
+    </View>
+  );
+}
+
+const predikat = (score) => {
+  var predikat = "";
+
+  if (score >= 86 && score <= 100) {
+    predikat = "Sangat Baik (A)";
+  } else if (score >= 71) {
+    predikat = "Baik (B)";
+  } else if (score >= 56) {
+    predikat = "Cukup (C)";
+  } else {
+    predikat = "Kurang (D)";
+  }
+
+  return predikat;
+}
