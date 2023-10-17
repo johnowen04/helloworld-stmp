496a44ec3c0a692fb95379cbc85dfa9cb1ca9f3a
John Owen <johnowen77777@gmail.com>
2023-10-18 00:15:00 +0700

    Latihan 2B

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={{ fontSize: 50, color: 'rgb(255,0,0)' }} >Hello, World!</Text>
       <Nilai score='99'></Nilai>
-      <Kotak size='50'></Kotak>
+      <Kotak size='50' red='255' green='0' blue='0'></Kotak>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,21 +24,63 @@ const styles = StyleSheet.create({
 
 const Kotak = (prop) => {
   const [action, setAction] = useState(prop.size);
+  const [red, setRed] = useState(prop.red);
+  const [green, setGreen] = useState(prop.green);
+  const [blue, setBlue] = useState(prop.blue);
 
   return (
-    <View>
-      <Text style={{ fontSize: action * 1, backgroundColor: 'rgb(255,0,0)' }}
+    <View style={{ flexDirection: 'row' }}>
+      <Text style={{ fontSize: action * 1, backgroundColor: 'rgb(' + red + ',' + green + ',' + blue + ')' }}
       >&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+
       <Button title="Smaller"
         onPress={() => {
           if (action - 5 > 0) {
             setAction(action * 1 - 5);
           }
         }} />
+
       <Button title="Bigger"
         onPress={() => {
           if (action + 5 <= 100) {
             setAction(action * 1 + 5);
+          }
+        }} />
+
+      <Button title="R+"
+        onPress={() => {
+          if (red + 10 <= 255) {
+            setRed(red * 1 + 10);
+          }
+        }} />
+      <Button title="R-"
+        onPress={() => {
+          if (red - 10 > 0) {
+            setRed(red * 1 - 10);
+          }
+        }} />
+      <Button title="G+"
+        onPress={() => {
+          if (green + 10 <= 255) {
+            setGreen(green * 1 + 10);
+          }
+        }} />
+      <Button title="G-"
+        onPress={() => {
+          if (green - 10 > 0) {
+            setGreen(green * 1 - 10);
+          }
+        }} />
+      <Button title="B+"
+        onPress={() => {
+          if (blue + 10 <= 255) {
+            setBlue(blue * 1 + 10);
+          }
+        }} />
+      <Button title="B-"
+        onPress={() => {
+          if (blue - 10 > 0) {
+            setBlue(blue * 1 - 10);
           }
         }} />
     </View>
